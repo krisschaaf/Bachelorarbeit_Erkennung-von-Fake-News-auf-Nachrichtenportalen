9fdc81c27785f435dad8bee6c1794b54b4e2d277
krisschaaf <kris-schaaf@gmx.de>
2025-05-07 11:14:41 +0200

    add rudimentary flask server and POC for browser extension

---
+++ Code/Chrome Extension/background.js
@@ -0,0 +1,9 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url.match('https:\/\/.*.bild.de\/.*') && changeInfo.status === 'complete') {
+        chrome.tabs.sendMessage(tabId, {
+            message: "background::urlChanged::bild",
+        })
+        console.log(`${tab.url} is the new URL from tabId ${tabId}`);
+    }
+}
+);
--- Code/Chrome Extension/bild.js
+++ Code/Chrome Extension/bild.js
@@ -1,34 +1,82 @@
-function analyseArticle() {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.message === "background::urlChanged::bild") {
+        console.log(`bildContentScript::messageReceived`)
+
+        analyseArticle().then(() => {
+            console.log("Article analysed");
+        }
+        ).catch(err => {
+            console.error("Error analysing article:", err);
+        });
+    }
+});
+
+const analyseArticle = async () => {
     const articles = document.getElementsByTagName("article");
-  
-    if (articles.length === 1) {
-      const article = articles[0];
-  
-      if (!document.getElementById("analyse-container")) {
+
+    if (articles.length !== 1) {
+        console.log("Article contains live-ticker...returning")
+        return;
+    };
+
+    const article = articles[0];
+    const headlineElement = article.querySelector("span.headline");
+    
+    if (!headlineElement) {
+        console.log("No headline found...returning")
+        return;
+    }
+
+    const headlineText = headlineElement.textContent.trim();
+    console.log(`headlineText: ${headlineText}`);
+
+    await fetchDataTest().then(data => {
+        appendDataToDOM(data, headlineText, article);
+    }).catch(err => {
+        console.error("Error fetching data:", err);
+    }
+    );
+}
+
+const fetchDataTest = async () => {
+    const response = await fetch("http://localhost:4000/test", {
+        method: "GET",
+        headers: { "Accept": "application/json" },
+    });
+
+    const data = await response.json();
+    return data;
+};
+
+
+const appendDataToDOM = (data, headlineText, article) => {
+    if (!document.getElementById("analyse-container")) {
+        console.log("Container not found, creating new one");
+
         const container = document.createElement("div");
         container.id = "analyse-container";
-      
+
         const p = document.createElement("p");
-        p.textContent = "Der Artikel wird analysiert...";
+        p.textContent = `${headlineText} and ${JSON.stringify(data)}`;
         p.id = "analyse-paragraph";
-      
+
         container.appendChild(p);
-      
         article.parentNode.insertBefore(container, article);
-      }
-      
     }
-  }
-  
-  analyseArticle();
-  
-  // Beobachte DOM-Änderungen für SPA-Seiten
-  const observer = new MutationObserver(() => {
-    analyseArticle();
-  });
-  
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-  
+}
+
+//   const async postArticle = (headlineText) => {
+//     const data = {};
+//     try {
+//         const res = await fetch("http://localhost:4000/test", {
+//             method: "POST",
+//             headers: { "Accept": "application/json" },
+//             body: JSON.stringify({ headline: headlineText })
+//         });
+//         data = await res.json();
+//         console.log("Response data:", data);
+//     } catch (err) {
+//         console.error("Fehler beim Abrufen der Daten:", err);
+//     }
+//     return data;
+//   }
