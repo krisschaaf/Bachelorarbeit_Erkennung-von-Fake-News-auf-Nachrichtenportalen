66fcdb6662f34fba580b3e27e11a38902f563c9a
krisschaaf <kris-schaaf@gmx.de>
2025-05-13 18:30:49 +0200

    fix git error

--- Code/Chrome Extension/contentScripts/backendService.js
+++
@@ -1,9 +0,0 @@
-globalThis.fetchDataTest = async () => {
-    const response = await fetch("http://localhost:4000/test", {
-        method: "GET",
-        headers: { "Accept": "application/json" },
-    });
-
-    const data = await response.json();
-    return data;
-};
