26741abe2125eafa521657ebd1d660b03de2d42e
krisschaaf <kris-schaaf@gmx.de>
2025-06-03 16:24:15 +0200

    create first combined dataset for mvp

--- Code/Machine Learning Model/flask/src/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-from flask_cors import CORS  
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/test', methods=['GET'])
-def test():
-  return jsonify({'message': 'The server is running'})
--- Code/Machine Learning Model/run_flask.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script is used to run the Flask application for the machine learning model.
-
-cd flask/src
-
-flask run --host=0.0.0.0 --port=4000
