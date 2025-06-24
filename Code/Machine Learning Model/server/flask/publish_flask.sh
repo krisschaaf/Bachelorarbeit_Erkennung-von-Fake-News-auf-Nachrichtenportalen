e94dfe1b35407bb3f68e52024455b6b27b4b6cd7
krisschaaf <kris-schaaf@gmx.de>
2025-06-24 22:42:06 +0200

    cleanup folders and files

--- Code/Machine Learning Model/server/flask/publish_flask.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo $ACCESS_TOKEN_GHCR | docker login ghcr.io -u $USERNAME_GHCR --password-stdin
-
-docker build -t ghcr.io/krisschaaf/erkennung_von_fake-news_auf_nachrichtenportalen/flask:latest .
-docker push ghcr.io/krisschaaf/erkennung_von_fake-news_auf_nachrichtenportalen/flask:latest
